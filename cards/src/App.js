97884626fb867e2d44e34140fd8c82fd7f5dbba2
Timo Feindt <t.feindt93@gmail.com>
2024-07-02 14:53:36 +0200

    add images and a little bit more styling

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,38 +1,39 @@
 import { useState } from "react";
 import Card from "./Card";
 import './index.css'
+import Ocean from './img/Ocean.jpg';
+import Desert from './img/Desert.jpg'
+import IceDesert from './img/iceDesert.jpg'
+import Mountain from './img/Mountain.jpg'
 
 function App() {
 
   const initialCardObject = [
     {
-      cardImage: '#',
-      cardHeadline: 'Headline',
+      cardImage: Ocean,
+      cardHeadline: 'Ocean',
       cardDescription: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti facere illo reiciendis aperiam aliquam aut fugit nihil odio ipsum voluptas suscipit quae ipsa, maxime impedit neque praesentium soluta quidem exercitationem.'
     }, 
     {
-      cardImage: '#',
-      cardHeadline: 'Headline 2',
+      cardImage: Desert,
+      cardHeadline: 'Desert',
       cardDescription: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti facere illo reiciendis aperiam aliquam aut fugit nihil odio ipsum voluptas suscipit quae ipsa, maxime impedit neque praesentium soluta quidem exercitationem.  Corrupti facere illo reiciendis aperiam aliquam aut fugit nihil odio ipsum voluptas suscipit quae ipsa, maxime impedit neque praesentium soluta quidem exercitationem.'
     }, 
     {
-      cardImage: '#',
-      cardHeadline: 'Headline 3',
+      cardImage: IceDesert,
+      cardHeadline: 'Ice',
       cardDescription: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti facere illo reiciendis aperiam aliquam aut fugit nihil odio ipsum voluptas suscipit quae ipsa, maxime impedit neque praesentium soluta quidem exercitationem.'
     }, 
     {
-      cardImage: '#',
-      cardHeadline: 'Headline 4',
+      cardImage: Mountain,
+      cardHeadline: 'Mountain',
       cardDescription: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti facere illo reiciendis aperiam aliquam aut fugit nihil odio ipsum voluptas suscipit quae ipsa, maxime impedit neque praesentium soluta quidem exercitationem.  Corrupti facere illo reiciendis aperiam aliquam aut fugit nihil odio ipsum voluptas suscipit quae ipsa, maxime impedit neque praesentium soluta quidem exercitationem.'
     }, 
-  ]
-
-  const [cardList, setCardList] = useState(initialCardObject)
-  
+  ] 
 
   return (
     <div className="container">
-      {cardList.map(card => 
+      {initialCardObject.map(card => 
         {
           return <Card card={card} />
         })  
