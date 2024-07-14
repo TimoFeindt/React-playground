e122cdc153557a94727d08751a8299b3570b982b
Timo Feindt <t.feindt93@gmail.com>
2024-07-14 22:21:33 +0200

    add loadingbar logic (can be refactored)

--- progressBar/src/components/ProgressBar.jsx
+++ progressBar/src/components/ProgressBar.jsx
@@ -1,21 +1,46 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function ProgressBar() {
 
-    const [barHeight, setBarHeight] = useState(10)
+    const [barWidth, setBarWidth] = useState(0)
+    const [buttonClicked, setButtonClicked] = useState(false)
+    const [barFull, setBarFull] = useState(false)
 
-    const handleBarHeight = () => {
-        setBarHeight(barHeight + 1);
+
+    useEffect(() => {
+        if(!buttonClicked) return
+        const interval = setInterval(()=> {
+            setBarWidth(prevValue => { 
+                    if(prevValue >= 400) {
+                        clearInterval(interval)
+                        return prevValue
+                    }
+                    return prevValue + 1;
+                }
+            );
+        }, 37)
+
+        return () => clearInterval(interval)
+    }, [buttonClicked])
+        
+    useEffect(() => {
+        if(barWidth === 400) {
+            setBarFull(true)
+        }
+    }, [barWidth])
+
+    const handleStartClick = () => {
+        setButtonClicked(!buttonClicked)
     }
 
     return(
-        <>
+        <div className="progressBar__box">
             <h3>Ich bin die ProgressBr</h3>
             <div className="progressBar">
-                <div className="progressBar__outer"></div>
-                <div className="progressBar__inner" style={{height: `${barHeight}px`}}></div>
+                <div className={`progressBar__outer ${barFull ? "barFull" : ''} `} ></div>
+                <div className="progressBar__inner" style={{width: `${barWidth}px`}}></div>
             </div>
-            <button onClick={handleBarHeight}>Increase Height</button>
-        </>
+            <button className="progressBar__button" onClick={handleStartClick}>Fill bar</button>
+        </div>
     )
 }
